4dfca2fd6989339cc8632535979946df83075e54
patelaftab <10patelaftab@gmail.com>
2023-01-16 20:30:19 +0530

    [Aftab] Adding Welcome Message

---
+++ IndianStatesSensus/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace IndiaStateSensus
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Indian State Sensus Data Analyser Program");
+            Console.ReadLine();
+        }
+    }
+}
